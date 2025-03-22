ffdcb28f9e5ab75493478a100274e3bfa3f7dcd1
Hérick Bittencourt <professional.herickvvb@gmail.com>
2025-03-22 14:16:29 -0300

    Feat: Fuzzy algorithm for movies

--- FuzzyMovies/src/FuzzyMain.java
+++ FuzzyMovies/src/FuzzyMain.java
@@ -16,18 +16,78 @@ public class FuzzyMain {
         System.out.println("Relevância total: " + result);
 		
         GrupoVariaveis grupoGenero = new GrupoVariaveis();
-        grupoGenero.add(new VariavelFuzzy("Genero Irrelevante", 0, 0, 10, 20));
-        grupoGenero.add(new VariavelFuzzy("Genero Pouco Relevante", 10, 20, 30, 60));
-        grupoGenero.add(new VariavelFuzzy("Genero Relevante", 20, 40, 50, 70));
-        grupoGenero.add(new VariavelFuzzy("Genero Muito Relevante", 40, 60, 70, 120));
+        grupoGenero.add(new VariavelFuzzy("Genero Irrelevante", -100, 0, 0, (float) 0.1));
+        grupoGenero.add(new VariavelFuzzy("Genero Pouco Relevante", (float) 0.0, (float) 0.1, (float) .5, (float) .75));
+        grupoGenero.add(new VariavelFuzzy("Genero Relevante", (float) .5, (float) .75, (float) .8, (float) 1));
+        grupoGenero.add(new VariavelFuzzy("Genero Muito Relevante", (float) .9, (float) .95, (float) .95, (float) 100));
         
         GrupoVariaveis grupoKeywords = new GrupoVariaveis();
-        grupoKeywords.add(new VariavelFuzzy("Keywords Irrelevantes", 0, 0, 10, 20));
-        grupoKeywords.add(new VariavelFuzzy("Keywords Pouco Relevantes", 10, 20, 30, 60));
-        grupoKeywords.add(new VariavelFuzzy("Keywords Relevantes", 20, 40, 50, 70));
-        grupoKeywords.add(new VariavelFuzzy("Keywords Muito Relevantes", 40, 60, 70, 120));
+        grupoKeywords.add(new VariavelFuzzy("Keywords Irrelevantes", 0, 0, 1, (float) 1.5));
+        grupoKeywords.add(new VariavelFuzzy("Keywords Pouco Relevantes", 1, 2, 2, (float) 2.5));
+        grupoKeywords.add(new VariavelFuzzy("Keywords Relevantes", 2, 3, (float) 3.5, 4));
+        grupoKeywords.add(new VariavelFuzzy("Keywords Muito Relevantes", 4, (float) 4.5, 10, 10));
         
-        
+        try {
+			BufferedReader bfr = new BufferedReader(new FileReader(new File("movie_dataset.csv")));
+			
+			String header = bfr.readLine();
+			String splitheder[] = header.split(";");
+			for (int i = 0; i < splitheder.length;i++) {
+				System.out.println(""+i+" "+splitheder[i]);
+			}
+			
+			String line = "";
+			
+			while((line=bfr.readLine())!=null) {
+				String spl[] = line.split(";");
+				HashMap<String,Float> asVariaveis = new HashMap<String,Float>();
+				
+				String genero = spl[2];
+				grupoGenero.fuzzifica(fuzzyGenreMatch(genero), asVariaveis);
+				
+				String keywords = spl[5];
+				grupoKeywords.fuzzifica(fuzzyKeywordMatch(keywords), asVariaveis);
+				
+				
+				System.out.println(""+spl[7]+" - generos: "+genero+" palavras-chave: "+ keywords);
+				
+				
+				rodaRegraE(asVariaveis,"Genero Irrelevante", "Keywords Irrelevantes", "Irrelevante");
+				rodaRegraE(asVariaveis,"Genero Irrelevante", "Keywords Pouco Relevantes", "Irrelevante");
+				rodaRegraE(asVariaveis,"Genero Irrelevante", "Keywords Relevantes", "Irrelevante");
+				rodaRegraE(asVariaveis,"Genero Irrelevante", "Keywords Muito Relevantes", "Relevante");
+				
+				rodaRegraE(asVariaveis,"Genero Pouco Relevante", "Keywords Irrelevantes", "Irrelevante");
+				rodaRegraE(asVariaveis,"Genero Pouco Relevante", "Keywords Pouco Relevantes", "Irrelevante");
+				rodaRegraE(asVariaveis,"Genero Pouco Relevante", "Keywords Relevantes", "Relevante");
+				rodaRegraE(asVariaveis,"Genero Pouco Relevante", "Keywords Muito Relevantes", "Relevante");
+				
+				rodaRegraE(asVariaveis,"Genero Relevante", "Keywords Irrelevantes", "Irrelevante");
+				rodaRegraE(asVariaveis,"Genero Relevante", "Keywords Pouco Relevantes", "Relevante");
+				rodaRegraE(asVariaveis,"Genero Relevante", "Keywords Relevantes", "Muito Relevante");
+				rodaRegraE(asVariaveis,"Genero Relevante", "Keywords Muito Relevantes", "Muito Relevante");
+				
+				rodaRegraE(asVariaveis,"Genero Muito Relevante", "Keywords Irrelevantes", "Relevante");
+				rodaRegraE(asVariaveis,"Genero Muito Relevante", "Keywords Pouco Relevantes", "Muito Relevante");
+				rodaRegraE(asVariaveis,"Genero Muito Relevante", "Keywords Relevantes", "Muito Relevante");
+				rodaRegraE(asVariaveis,"Genero Muito Relevante", "Keywords Muito Relevantes", "Muito Relevante");
+				
+				float Irrelevante = asVariaveis.get("Genero Irrelevante");
+				float Relevante = asVariaveis.get("Relevante");
+				float MuitoRelevante = asVariaveis.get("Muito Relevante");
+				
+				float score = (Irrelevante*1.5f+Relevante*7.0f+MuitoRelevante*9.5f)/(Irrelevante+Relevante+MuitoRelevante);
+				
+				System.out.println("Irrelevante: "+ Irrelevante+" Relevante: "+ Relevante +" Muito Relevante: "+ MuitoRelevante);
+				System.out.println(" "+genero+" | "+keywords +"-> "+score);
+				System.out.println(" ");
+			}
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
 
 	private static void rodaRegraE(HashMap<String, Float> asVariaveis,String var1,String var2,String varr) {
